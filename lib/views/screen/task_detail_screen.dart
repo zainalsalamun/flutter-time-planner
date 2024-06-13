dc2e7ec0f6c7625bef4593213c659115c6502666
binar-rizki <zainalrtf@gmail.com>
2024-06-13 14:11:48 +0700

    add task detail screen

---
+++ lib/views/screen/task_detail_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../../data/model/task_model.dart';
+
+class TaskDetailScreen extends StatelessWidget {
+  final TaskModel task;
+  const TaskDetailScreen({super.key, required this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
